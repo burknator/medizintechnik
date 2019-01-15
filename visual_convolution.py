777f622cd2e487341a4055e048b3a6b3e0e03581
Patrick Burke <hi@pburke.de>
2019-01-15 14:16:16 +0100

    Runde Ausgaben der Matrizen

--- visual_convolution.py
+++ visual_convolution.py
@@ -7,11 +7,14 @@ from IPython.display import HTML, display, Math, Latex
 
 def show_matrix(data, title=None, mark=set()):
     # Helper-method to show a matrix
+    data = np.around(data).astype(int)
     return display(Latex(r'\[\scriptstyle ' + (title.replace('_', '\_') + ' = ' if title else '') + r' \def\g{\color{lightgray}}\left(\begin{array}{%s}' % (r'r'*data.shape[0]) + r'\\'.join([r' & '.join([ value if (x, y) in mark or not mark else r'\g{%s}' % value for (y, value) in enumerate(map(str, col))]) for (x, col) in enumerate(data)]) + r'\end{array}\right)\]'))
 
 def show_matrix_with_kernel_and_result(data, kernel, kernel_x, kernel_y, result):
     # Helper-method to show a matrix
     mark = set([(i, j) for i in range(kernel_x-1, kernel_x+2) for j in range(kernel_y-1, kernel_y + 2) ])
+    data = np.around(data).astype(int)
+    result = np.around(result).astype(int)
     data = data.astype(str)
 
     for (x, y) in mark:
@@ -46,7 +49,7 @@ def convolve(image, kernel, debug=False, frames=None, stop_after_steps=None, vis
         window = _image[x:x+3, y:y+3] # image[x-1:x+2, y-1:y+2] but x=x+1, y=y+1 because of pad
         output_image[x, y] = np.sum(np.multiply(window, kernel))
         if visualize:
-            plt.imshow(output_image, cmap="gray", norm=NoNorm())
+            #plt.imshow(output_image, cmap="gray", norm=NoNorm())
             #show_matrix(_image, set([(i, j) for i in range(x, x+3) for j in range(y, y + 3) ]))
             show_matrix_with_kernel_and_result(_image, kernel, x+1, y+1, output_image)
         if frames:
